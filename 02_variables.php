94abfb3e933e8d453f244594427db39d4644c7a0
Daniel Molnar <molnard.work@gmail.com>
2022-12-15 10:20:51 +0100

    Variables

---
+++ 02_variables.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = 'Brad';
+$age = 40;
+$has_kids = true;
+$cash_on_hands = 20.75;
+
+echo $name;
+var_dump($has_kids);
+echo "${name} is ${age} years old";
+
+echo '5' + '5';
+
+define('HOST', 'localhost');
+
+
+?>
